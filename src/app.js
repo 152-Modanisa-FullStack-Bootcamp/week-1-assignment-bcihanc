9f34ca2f5513829253ffc3ec5fa4e21300ca09f6
bcihanc <b.cihancengiz@gmail.com>
2021-12-29 14:27:22 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -2,28 +2,49 @@ import "./styles.css";
 import axios from "axios";
 
 axios
-  .get("https://my-json-server.typicode.com/modanisatech/bootcamp-db/products")
-  .then((response) => {
-    // Firstly, log response to the console,
-    // inspect the response and see that it has data field
+    .get("https://my-json-server.typicode.com/modanisatech/bootcamp-db/products")
+    .then((response) => {
+        // Firstly, log response to the console,
+        // inspect the response and see that it has data field
+        console.log(response);
 
-    // Assign data field of the response to
-    // products variable below by destructuring
-    // You can use alias
-    const products = null;
+        // Assign data field of the response to
+        // products variable below by destructuring
+        // You can use alias
+        const {data: products} = response;
 
-    // Print names of all product to the console
-    // by calling foreach  method (use arrow function)
+        // Print names of all product to the console
+        // by calling foreach  method (use arrow function)
+        products.forEach((product) => console.log(product.name)); // or product["name"]
 
-    // Get all products that contain "Şal" in their name (use filter method)
-    // map filtered products to new object having only image and name field
-    // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+        // Get all products that contain "Şal" in their name (use filter method)
+        // map filtered products to new object having only image and name field
+        // assign mapped items to mappedProducts variable
+        const mappedProducts = products
+            .filter((product) => product["name"].includes("Şal"))
+            .map((product) => ({name: product.name, image: product.image}));
 
-    // Display the images and names of mappedProducts
-    // You need to add them to the DOM
-    // you need to use forEach method
-    // You need to use flexbox
-    // Position of image and text is up to you
-    // You can use any style you wish
-  });
+        // Display the images and names of mappedProducts
+        // You need to add them to the DOM
+        // you need to use forEach method
+        // You need to use flexbox
+        // Position of image and text is up to you
+        // You can use any style you wish
+        for (const product of mappedProducts) {
+            const productCard = document.createElement("div");
+            productCard.setAttribute("id", "product-card")
+
+            const productImage = document.createElement("img");
+            productImage.setAttribute("id", "product-image");
+            productImage.src = product.image;
+
+            const productName = document.createElement("div");
+            productName.setAttribute("id", "product-name");
+            productName.append(document.createTextNode(product.name));
+
+            productCard.appendChild(productImage);
+            productCard.appendChild(productName);
+
+            document.getElementById("flex-container").appendChild(productCard);
+        }
+    });
